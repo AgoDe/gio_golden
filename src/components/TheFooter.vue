bdb33f4536e9e1157e0077fc6da65cf3d1800997
AgoDe <ago.defelice@gmail.com>
2022-05-07 22:24:17 +0200

    print footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,76 @@
+<template>
+    <footer>
+
+        <div class="container">
+            <div class="row">
+
+                <!-- info -->
+                <div class="col-12 col-lg-6 info">
+                    <img src="/images/Negativo_B.png" alt="logo">
+                </div>
+
+                <!-- loghi certificati -->
+                <div class="col-12 col-lg-6 loghi">
+                    <img src="/images/logo_fci.png" alt="logo_fci">
+                    <img src="/images/logo-enci.png" alt="logo_enci">
+                    <img src="/images/logo-kennel.png" alt="logo_kennel">
+                </div>
+            </div>
+        </div>
+
+        <!-- logo ADF -->
+        <div class="adf">
+            <span>Made by</span>
+            <img src="/images/logo-square-bw.png" alt="logo-adf">
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'bootstrap';
+@import '/src/styles/main.scss';
+
+footer {
+    background: $yellow;
+
+    .container {
+
+        padding: 30px 0;
+        .row {
+
+            .info {
+                img {
+                    height: 60px;
+                }
+            }
+
+            .loghi {
+                display: flex;
+                justify-content: space-around;
+
+                img {
+                    width: 100px;
+                }
+            }
+        } // end of row
+    } // end of container
+
+    
+    .adf {
+        background: $orange;
+        text-align: center;
+        
+
+        img {
+            height: 50px;
+        }
+    }
+}
+
+</style>
--- src/pages/HomeContainer.vue
+++ src/pages/HomeContainer.vue
@@ -6,25 +6,39 @@
         <the-jumbo/>
 
         <div class="container">
-            
+
         </div>
+
+        <the-footer/>
     </div>
 
 
 </template>
 
 <script>
+import TheFooter from '../components/TheFooter.vue'
 import TheJumbo from '../components/TheJumbo.vue'
 import TheNavbar from '../components/TheNavbar.vue'
 export default {
     components: { 
         TheNavbar, 
-        TheJumbo 
+        TheJumbo,
+        TheFooter 
     },
     
 }
 </script>
 
 <style lang="scss" scoped>
+@import 'bootstrap';
+@import '/src/styles/main.scss';
+
+.home-container {
+   
+
+   .container {
+       height: 200px;
+   }
+}
 
 </style>
