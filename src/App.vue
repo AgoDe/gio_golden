d6dfdc9cd1523e6c4ca17b5a23fb03b70b1e9e7c
AgoDe <ago.defelice@gmail.com>
2022-05-07 11:50:05 +0200

    print jumbotron

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
   
       <the-navbar/>
 
+      <the-jumbo/>
+      
   </div>
 </template>
 
 <script>
+import TheJumbo from './components/TheJumbo.vue'
 import TheNavbar from './components/TheNavbar.vue'
 
 
 export default {
   name: 'App',
   components: {
-    TheNavbar
+    TheNavbar,
+    TheJumbo
     
   }
 }
---
+++ src/components/TheJumbo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="jumbotron">
+
+      <!-- <img src="https://via.placeholder.com/2000x600" alt="placeholder"> -->
+        <div class="container">
+            <div class="row">
+                <img src="/images/primario.png" alt="logo">
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'bootstrap';
+@import '/src/styles/main.scss';
+
+    .jumbotron {
+       background: url('https://via.placeholder.com/2000x600') center center;
+       height: 600px;
+       border-bottom: $lineWeight $orange solid;
+
+       .row {
+
+           position: relative;
+
+           img {
+               width: 400px;
+               position: relative;
+               top: 485px;
+           }
+       }
+    }
+</style>
