e752a277637a45c4619ab9df1f1fb2c44b1bbe2e
AgoDe <ago.defelice@gmail.com>
2022-05-08 22:58:59 +0200

    add animation to jumbo logo

--- src/components/TheJumbo.vue
+++ src/components/TheJumbo.vue
@@ -4,14 +4,32 @@
       <!-- <img src="https://via.placeholder.com/2000x600" alt="placeholder"> -->
         <div class="container">
             <div class="row">
-                <img src="/images/primario.png" alt="logo">
+                <img class="logo" src="/images/primario.png" alt="logo">
             </div>
         </div>
   </div>
 </template>
 
 <script>
+import anime from 'animejs/lib/anime.es.js';
 export default {
+    mounted() {
+        //  anime({
+        //     targets: ['.enter', '.home'],
+        //     translateY: '-100%',
+        //     easing: 'steps(8)',
+        //     duration: 1500,
+        //     });
+
+        anime({
+
+            targets: '.logo',
+            scale: [40,1],
+            opacity: [0,1],
+            easing: "easeOutCirc",
+            duration: 2000,
+        })
+    }
 
 }
 </script>
